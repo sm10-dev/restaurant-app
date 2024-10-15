eb391c4dc40c82b1ea83e90c0b105a8c1b1dc755
sm10-dev <sm10.dev@gmail.com>
2024-10-15 15:18:06 +0530

    Modified menu table schema - Soumya

--- database/menu.sql
+++ database/menu.sql
@@ -2,5 +2,8 @@ CREATE TABLE menu (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price DECIMAL(5, 2) NOT NULL,
-  description TEXT
+  description TEXT,
+  Stars INT,
+  Ingredients TEXT,
+  Type VARCHAR(255)
 );
