f64691d4e47d49a2f20f4fd34e4aa19ce3d00a76
anishekchaudhary <anishekchaudhary@gmail.com>
2024-10-15 15:12:37 +0530

    modified the menu table schema

--- database/menu.sql
+++ database/menu.sql
@@ -2,5 +2,8 @@ CREATE TABLE menu (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price DECIMAL(5, 2) NOT NULL,
-  description TEXT
+  description TEXT,
+  ingedients TEXT NOT NULL,
+  Rating INT,
+  Category VARCHAR
 );
